bcdc9e9b4665666a320295a10c2b993c8f683f65
Anish Rathod <anishr6353@gmail.com>
2024-11-15 10:55:19 +0530

    portfolio-11 website

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector("#menu_btn");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
